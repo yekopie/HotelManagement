400a5abde999bcfd527c1b3cacd99ba104952d60
yekopie <yekuniverse@gmail.com>
2025-05-26 22:24:01 +0300

    Ef bağlantısı yapıldı

---
+++ DataAccess/Context/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Guest> Guests { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Maintenance> Maintenances { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<RoomType> RoomTypes { get; set; }
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+    }
+}
