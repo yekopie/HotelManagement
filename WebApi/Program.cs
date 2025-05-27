f6dcc559b9297c53b0f6b60975a22883783f40ea
yekopie <yekuniverse@gmail.com>
2025-05-27 22:23:37 +0300

    Paket versiyonları düzeltildi

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,7 @@
 
 using DataAccess.Context;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace WebApi
 {
@@ -11,13 +12,12 @@ namespace WebApi
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
             var app = builder.Build();
                 
             // Configure the HTTP request pipeline.
