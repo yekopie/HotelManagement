84cbd3bdff8442d5b78fb3b0b62d8f18161da699
yekopie <yekuniverse@gmail.com>
2025-05-30 00:05:39 +0300

    tip değişikliği

--- Entities/Concrete/Room.cs
+++ Entities/Concrete/Room.cs
@@ -10,8 +10,8 @@
         public int Floor { get; set; }
         public string RoomNumber { get; set; }
         public decimal PricePerNight { get; set; }
-        public byte IsAvailable { get; set; }
-        public byte IsClean { get; set; }
+        public bool IsAvailable { get; set; }
+        public bool IsClean { get; set; }
         public DateTime CreatedAt { get; set; }
         public virtual ICollection<Reservation> Reservations { get; set; }
         public virtual ICollection<Maintenance> Maintenances { get; set; }
