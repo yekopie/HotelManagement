5819e8e085884a1818a7572ebad8c02fb2ef58ed
yekopie <yekuniverse@gmail.com>
2025-05-30 00:06:15 +0300

    migrastion güncellendi

---
+++ DataAccess/Migrations/20250529203950_mig6.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig6 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsClean",
+                table: "Rooms",
+                type: "bit",
+                nullable: false,
+                defaultValue: true,
+                oldClrType: typeof(byte),
+                oldType: "tinyint",
+                oldDefaultValue: (byte)1);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsAvailable",
+                table: "Rooms",
+                type: "bit",
+                nullable: false,
+                defaultValue: true,
+                oldClrType: typeof(byte),
+                oldType: "tinyint",
+                oldDefaultValue: (byte)1);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte>(
+                name: "IsClean",
+                table: "Rooms",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)1,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldDefaultValue: true);
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "IsAvailable",
+                table: "Rooms",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)1,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldDefaultValue: true);
+        }
+    }
+}
